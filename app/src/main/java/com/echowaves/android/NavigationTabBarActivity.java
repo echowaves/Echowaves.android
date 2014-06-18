d40ea8bab3a250ccb7bed33d5f3905e7dc45ff0c
dmitryame <dmitry@custommode.com>
2014-06-18 17:31:04 -0400

    WIP: adding tabs

--- app/src/main/java/com/echowaves/android/NavigationTabBarActivity.java
+++ app/src/main/java/com/echowaves/android/NavigationTabBarActivity.java
@@ -1,17 +1,29 @@
 package com.echowaves.android;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 
-public class NavigationTabBarActivity extends ActionBarActivity {
+public class NavigationTabBarActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_navigation_tab_bar);
+
+        TabHost tabs=(TabHost)findViewById(R.id.tabhost);
+        tabs.setup();
+        TabHost.TabSpec spec=tabs.newTabSpec("tag1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Clock");
+        tabs.addTab(spec);
+        spec=tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Button");
+        tabs.addTab(spec);
     }
 
 
