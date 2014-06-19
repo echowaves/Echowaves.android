369209d436e0b5c016395fe2da847eb673a2fde3
dmitryame <dmitry@custommode.com>
2014-06-18 22:19:39 -0400

    WIP: adding tabs (cleanup)

--- app/src/main/java/com/echowaves/android/BaseActivity.java
+++ app/src/main/java/com/echowaves/android/BaseActivity.java
@@ -5,16 +5,19 @@ import android.app.Activity;
 import com.flurry.android.FlurryAgent;
 
 /**
- * Created by dmitry on 6/14/14.
+ * copyright echowaves
+ * Created by dmitry
+ * on 6/14/14.
  */
+
 public class BaseActivity extends Activity {
-    public static String FluryKey = "V4V86GZNZGPXMHR9FM6Q";
+    public static String FlurryKey = "V4V86GZNZGPXMHR9FM6Q";
 
     @Override
     protected void onStart()
     {
         super.onStart();
-        FlurryAgent.onStartSession(this, BaseActivity.FluryKey);
+        FlurryAgent.onStartSession(this, BaseActivity.FlurryKey);
     }
 
     @Override
--- app/src/main/java/com/echowaves/android/EchoWaveActivity.java
+++ app/src/main/java/com/echowaves/android/EchoWaveActivity.java
@@ -1,8 +1,6 @@
 package com.echowaves.android;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class EchoWaveActivity extends BaseActivity {
 
@@ -12,20 +10,4 @@ public class EchoWaveActivity extends BaseActivity {
         setContentView(R.layout.activity_echo_wave);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.echo_wave, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
-    }
 }
