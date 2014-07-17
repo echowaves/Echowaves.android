52a62f6044b03b408d8e2d943a320f5b57a88d89
dmitryame <dmitry@custommode.com>
2014-07-16 22:59:04 -0400

    storing last update time in prefs

--- app/src/main/java/com/echowaves/android/WavingTabFragment.java
+++ app/src/main/java/com/echowaves/android/WavingTabFragment.java
@@ -1,6 +1,7 @@
 package com.echowaves.android;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,7 +9,40 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.echowaves.android.model.ApplicationContextProvider;
+import com.echowaves.android.model.SecurePreferences;
+
+import java.text.DateFormat;
+import java.util.Date;
+
 public class WavingTabFragment extends EWTabFragment {
+    static final private SharedPreferences prefs = new SecurePreferences(ApplicationContextProvider.getContext());
+
+    public static final String CURRENT_ASSET_DATE_TIME_KEY = "currentAssetDateTime";
+
+    private static Date currentAssetDateTime;
+
+    public static Date getCurrentAssetDateTime() {
+        if(currentAssetDateTime == null) {
+            long storedTime = prefs.getLong(CURRENT_ASSET_DATE_TIME_KEY, 0);
+            if(storedTime == 0) {
+                storedTime = (new Date()).getTime();
+                setCurrentAssetDateTime(new Date(storedTime));
+            }
+            currentAssetDateTime = new Date(storedTime);
+        }
+        return currentAssetDateTime;
+    }
+
+    public static void setCurrentAssetDateTime(Date currentAssetDateTime) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong(CURRENT_ASSET_DATE_TIME_KEY, currentAssetDateTime.getTime());
+        editor.commit();
+        WavingTabFragment.currentAssetDateTime = currentAssetDateTime;
+    }
+
+
+
 
     @Override
     public void onStart() {
@@ -22,6 +56,22 @@ public class WavingTabFragment extends EWTabFragment {
         View view = inflater.inflate(R.layout.fragment_waving, container, false);
         Log.d("WavingTabFragment", view.toString());
 
+        Button sinceDateTimeValueButton = (Button) view.findViewById(R.id.waving_sinceDateTimeValueButton);
+
+        sinceDateTimeValueButton.setText(DateFormat.getDateTimeInstance(
+                DateFormat.MEDIUM, DateFormat.SHORT).format(getCurrentAssetDateTime()));
+        //Listening to button event
+        sinceDateTimeValueButton.
+                setOnClickListener(
+                        new View.OnClickListener() {
+                            public void onClick(final View v) {
+//                                Intent addChildWaveIntent = new Intent(v.getContext(), AddChildWaveActivity.class);
+//                                startActivity(addChildWaveIntent);
+                            }
+                        }
+                );
+
+
         Button addWaveButton = (Button) view.findViewById(R.id.waving_addNewWaveButton);
         //Listening to button event
         addWaveButton.
@@ -33,6 +83,8 @@ public class WavingTabFragment extends EWTabFragment {
                             }
                         }
                 );
+
+
         return view;
     }
 
